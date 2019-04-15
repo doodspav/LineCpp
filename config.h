a2057034779efe8791c6d1b696abbfe9d448fa8a
doodspav <doods9doods@gmail.com>
2019-04-16 00:08:46 +0100

    Update config.h

--- config.h
+++ config.h
@@ -2,14 +2,22 @@
 #define LINE_CONFIG_H
 
 #include <cstdint>
+#include <string_view>
 
 namespace config
 {
-	constexpr char* HOST_NAME = "legy-jp.line.naver.jp";
+	// host & ip
+	constexpr std::string_view HOST_NAME = "legy-jp.line.naver.jp";
 	constexpr uint_fast16_t PORT = 443;
-	constexpr char* ENDPOINT = "/S4";
-	constexpr char* USER_AGENT = "LI/7.150 iPad6,3 10.2";
-	constexpr char* X_LINE_APPLICATION = "IOS\t8.2.4\tiOS\t10.2";
+
+	// endpoints
+	constexpr std::string_view NORMAL_ENDPOINT = "/S4";
+	constexpr std::string_view POLL_ENDPOINT = "/P4";
+	constexpr std::string_view AUTH_ENDPOINT = "/api/v4p/rs";
+
+	// user_agent & app_version combinations
+	constexpr std::string_view USER_AGENT = "LI/7.150 iPad6,3 10.2";
+	constexpr std::string_view X_LINE_APPLICATION = "IOS\t8.2.4\tiOS\t10.2";
 }
 
 #endif // !LINE_CONFIG
