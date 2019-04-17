1d25e104c13962edc26e4cb0fe1888813c6884a1
doodspav <doods9doods@gmail.com>
2019-04-17 15:41:11 +0100

    Create type_aliases.h

---
+++ Utils/type_aliases.h
@@ -0,0 +1,11 @@
+#ifndef TYPE_ALIASES_H
+#define TYPE_ALIASES_H
+
+#include <string>
+#include <tuple>
+#include <unordered_map>
+
+using string_pair = std::tuple<std::string, std::string>;
+using string_pair_umap = std::unordered_map<std::string, std::string>;
+
+#endif // !TYPE_ALIASES_H
